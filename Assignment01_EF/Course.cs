cdd7fc5472abafe77f745b93085276ee72d7fd40
Your Name <you@example.com>
2025-02-26 20:25:28 +0200

    Answer

---
+++ Assignment01_EF/Course.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment01_EF
+{
+    class Course
+    {
+        public int Id { get; set; }
+        public int Duration { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Top_Id { get; set; }
+
+    }
+}
---
+++ Assignment01_EF/Data/ITI_Context.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment01_EF.Data
+{
+    class ITI_Context:DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Topic> Topics { get; set; }
+        public DbSet<Instructor> Instructors { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+            optionsBuilder.UseSqlServer("Server=RASHED\\SQLEXPRESS;database=ITI_Ass01;Integrated Security=True;Encrypt=False;Trust Server Certificate=True");
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
